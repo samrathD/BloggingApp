bc52da2958b4c0ee6241b15468ac19a4002ab6c7
samrathD <samrath2004@gmail.com>
2024-08-24 15:01:57 -0700

    Project set up

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const PORT = 8080
+
+const userRoute = require("./routes/user")
+
+mongoose.connect('mongodb://localhost:27017/blogify').then((e)=>{console.log("MongoDB Connected")})
+// Set up view engines
+app.set("view engine","ejs")
+app.set("views", path.resolve("./views"))
+
+app.use(express.urlencoded({extended:false}))
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+app.use("/user",userRoute)
+
+app.listen(PORT,()=>{console.log(`Server Started at ${PORT }`)})
---
+++ routes/user.js
@@ -0,0 +1,38 @@
+const {Router} = require("express")
+const User = require("../models/user")
+const router = Router()
+
+// Get route
+router.get("/signin",(req,res)=>{
+    res.render("signin")
+})
+router.get("/signup",(req,res)=>{
+    res.render("signup")
+})
+
+// Post route
+router.post("/signin",async(req,res)=>{
+    const{email,password} = req.body
+    try{
+        const token = await User.matchPasswordAndGenerateToken(email,password)
+        return res.cookie("token",token).redirect("/")
+    }catch(error){
+        return res.render('signin',{
+            error:"Incorrect Email or Password"
+        })
+    }
+    
+})
+router.post("/signup", async(req,res)=>{
+    const {fullName,email,password} = req.body
+    await User.create({
+        fullName,
+        email,
+        password
+    })
+    return res.redirect("/")
+});
+
+
+
+module.exports = router
