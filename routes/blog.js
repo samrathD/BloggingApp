d65f57c31d2fbe24fe8dbced905f0310de358902
samrathD <samrath2004@gmail.com>
2024-08-28 18:57:24 -0700

    show complete blog and add commenting functionality

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const router = Router();
 const multer = require('multer')
 const path = require("path");
 const Blog = require("../models/blog");
+const Comment = require("../models/comments");
 
 
 const storage = multer.diskStorage({
@@ -36,6 +37,28 @@ router.post('/', upload.single("coverImage"), async (req,res)=>{
         createdBy
    })
    return res.redirect(`/blog/${blog._id}`)
+})
+
+router.get("/:id",async(req,res)=>{
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({blogId:req.params.id}).populate("createdBy")
+  console.log(comments)
+  return res.render("blog",{
+    user: req.user,
+    blog: blog,
+    comments: comments,
+  })
+})
+
+// Comment routes
+
+router.post("/comment/:blogId", async(req,res)=>{
+  const comment = Comment.create({
+    content:req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id
+  })
+  return res.redirect(`/blog/${req.params.blogId}`)
 })
 
 module.exports = router
